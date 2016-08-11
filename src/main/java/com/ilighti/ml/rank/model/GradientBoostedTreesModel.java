3899fbcc3393b741056e4603deddc600f453c570
fangxiaoyu <fangxiaoyu@waqu.com>
2016-08-11 15:25:26 +0800

    vote predict method

--- src/main/java/com/ilighti/ml/rank/model/GradientBoostedTreesModel.java
+++ src/main/java/com/ilighti/ml/rank/model/GradientBoostedTreesModel.java
@@ -3,6 +3,8 @@ package com.ilighti.ml.rank.model;
 import com.github.fommil.netlib.BLAS;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by rain on 16-8-9.
@@ -72,5 +74,60 @@ public class GradientBoostedTreesModel implements Serializable {
         }
         BLAS bias = com.github.fommil.netlib.BLAS.getInstance();
         return bias.ddot(numTrees, treePredictions, 1, treeWeights, 1);
+    }
+
+    public Double predictByVoting(Vector feature) {
+        Map<Integer, Double> votes = new HashMap<Integer, Double>();
+        int k = 0;
+        for(DecisionTreeModel tree : trees) {
+            Integer prediction = (int)tree.predict(feature);
+            if(votes.containsKey(prediction)) {
+                votes.put(prediction, votes.get(prediction) + treeWeights[k]);
+            } else {
+                votes.put(prediction, treeWeights[k]);
+            }
+            k++;
+        }
+        //max voted label
+        Integer label = 0;
+        Double maxVotes = Double.valueOf(-Float.MIN_VALUE);
+        for(Map.Entry<Integer, Double> entry : votes.entrySet()) {
+            if(maxVotes < entry.getValue()) {
+                maxVotes = entry.getValue();
+                label = entry.getKey();
+            }
+        }
+        return label.doubleValue();
+    }
+
+    private double sumOfWeights() {
+        double sum = 0.;
+        for(double w : treeWeights) {
+            sum += w;
+        }
+        return sum;
+    }
+
+    public Double predict(Vector feature) {
+        if (algo.equals(Algo.Regression) && strategy.equals(EnsembleCombiningStrategy.Sum)) {
+            return predictBySumming(feature);
+        } else if (algo.equals(Algo.Regression) && strategy.equals(EnsembleCombiningStrategy.Average)) {
+            return predictBySumming(feature) / sumOfWeights();
+        } else if (algo.equals(Algo.Classification) && strategy.equals(EnsembleCombiningStrategy.Sum)) {
+            Double prediction = predictBySumming(feature);
+            return prediction > 0. ? 1. : 0.;
+        } else if (algo.equals(Algo.Classification) && strategy.equals(EnsembleCombiningStrategy.Vote)) {
+            return predictByVoting(feature);
+        } else {
+            throw new RuntimeException("TreeEnsembleModel given unsupported (" + algo + "," + strategy + ")");
+        }
+    }
+
+    public Double[] predict(Vector[] features) {
+        Double[] ret = new Double[features.length];
+        for (int i = 0; i < features.length; i++) {
+            ret[i] = predict(features[i]);
+        }
+        return ret;
     }
 }
